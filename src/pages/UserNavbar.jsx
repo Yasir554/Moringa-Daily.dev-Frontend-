197043bc17d5908332721d41c1db74ec071603b4
Yasir554 <yasirabass554@gmail.com>
2025-04-30 16:46:08 +0300

    first commit

---
+++ src/pages/UserNavbar.jsx
@@ -0,0 +1,9 @@
+import {link} from "react-router-dom";
+
+const Navbar = () => {
+    return (<>
+    <Link to="/login" className="navbar-brand"> Login </Link>
+    </>)
+    }
+
+export default Navbar;
